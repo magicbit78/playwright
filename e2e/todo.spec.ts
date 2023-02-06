002b963b71a72ea4e7546917b6b992802c03f1fb
Stephen.Fang <stephen.fang@fleetcomplete.com>
2023-02-05 21:05:56 -0500

    tech-assessment: created 3rd test for deleting an existing item from toDo list

--- e2e/todo.spec.ts
+++ e2e/todo.spec.ts
@@ -40,11 +40,13 @@ test('Create a new todo item', async ({ page }) => {
 });
 
 test('Edit an existing todo item', async ({ page }) => {
+  // Initiate 2 items in todo list
   for (const item of TODO_ITEMS) {
     await page.locator('.new-todo').fill(item);
     await page.locator('.new-todo').press('Enter');
   }
 
+  // edit the 2nd todo item
   const todoItems = page.locator('.todo-list li');
   const secondTodo = todoItems.nth(1);
   await secondTodo.dblclick();
@@ -52,12 +54,33 @@ test('Edit an existing todo item', async ({ page }) => {
   await secondTodo.locator('.edit').fill('modified item two');
   await secondTodo.locator('.edit').press('Enter');
 
-  // Verify the 2nd item has the new value.
+  // Verify the list contains 2 todo items and the 2nd todo item has a new value.
   await expect(todoItems).toHaveText([TODO_ITEMS[0], 'modified item two']);
   await expect(page.locator('.view label')).toHaveText([TODO_ITEMS[0], 'modified item two']);
   await expect(page.locator('.todo-count')).toHaveText('2 items left');
+
   await checkNumberOfTodosInLocalStorage(page, 2);
   await checkTodosInLocalStorage(page, 'modified item two');
+});
+
+test('Delete a todo item using the red X', async ({ page }) => {
+  // Initiate 2 items in todo list
+  for (const item of TODO_ITEMS) {
+    await page.locator('.new-todo').fill(item);
+    await page.locator('.new-todo').press('Enter');
+  }
+
+  // Delete the first todo item using red X
+  const todoItems = page.locator('.todo-list li');
+  const secondTodo = todoItems.nth(0);
+  await secondTodo.locator('.destroy').dispatchEvent('click');
+
+  // Verify the 2nd todo item is the only one remaining in the list
+  await expect(page.locator('.todo-count')).toHaveText('1 item left');
+  await expect(page.locator('.view label')).toHaveText(TODO_ITEMS[1]);
+
+  await checkNumberOfTodosInLocalStorage(page, 1);
+  await checkTodosInLocalStorage(page, TODO_ITEMS[1]);
 });
 
 async function checkNumberOfTodosInLocalStorage(page: Page, expected: number) {
